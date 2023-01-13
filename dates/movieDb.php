fe7215461307643b27f188222b430eb31ba0d415
ManganiBiagio <biagiomangani@live.it>
2023-01-13 20:16:48 +0100

    add db

---
+++ dates/movieDb.php
@@ -0,0 +1,28 @@
+<?php
+$movieListInfo=[
+    [
+      "title"=>"indiana jones",
+      "actors"=>["Harrison Ford","TheRock"],
+      "year_production"=>1990,
+      "vote"=>5,
+      "generes"=>["action","drammatic"]
+
+    ],
+    [
+        "title"=>"spiderman",
+        "actors"=>["Tobey Maguire","Tom Holland"],
+        "year_production"=>2022,
+        "vote"=>4.5,
+        "generes"=>["romantic"]
+  
+    ],
+    [
+        "title"=>"signore degli anelli",
+        "actors"=>["Elijah Wood"],
+        "year_production"=>1999,
+        "vote"=>5,
+        "generes"=>["comedy"]
+  
+      ]
+
+];
--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <?php
 require_once "./classes/Movie.php";
 require_once "./classes/Generes.php";
+require_once "./dates/movieDb.php";
 
 
 $myMovie=new Movie("prova",["pippo","pluto"],1940,4.5);
 $yourMovie=new Movie("indiana jones",["Harrison Ford"],2022,5);
 $gen_action=new Generes("action");
 $gen_romantic=new Generes("romantic");
-$myMovie->setGeneres([$gen_action,"dsdasda"]);
-var_dump($myMovie);
-var_dump($yourMovie);
+$myMovie->setGeneres([$gen_action]);
+//var_dump($myMovie);
+//var_dump($yourMovie);
+
+$movieList=[];
+foreach($movieListInfo as $movie){
+    $newMovie=new Movie($movie["title"],$movie["actors"],$movie["year_production"],$movie["vote"]);
+    //errore nel creare i generi
+    $newMovie->setGeneres([new Generes($movie["generes"])]);
+    $movieList[]=$newMovie;
+
+}
+
+var_dump($movieList);
 
 
 ?>
